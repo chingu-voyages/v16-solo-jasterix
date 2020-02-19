bb51fdde599db73184674b4fe405684e7e55239f
Jasterix <ljcalixte@gmail.com>
2020-02-18 19:39:29 -0500

    add API/models/

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 app.listen(3000, () => console.log("Server has started"));
-// app.use(express.json())
 
-// require('dotenv').config()
+// Lets server accept json
+app.use(express.json());
 
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
 // const db = mongoose.connection;
-// db.on("error", error => console.error(error));
+
+db.once("open", () => console.log("Connected to Database"));
 // db.once("open", () => console.log("Connected to Database"));
+
+// db.on("error", console.error(error));
+db.on("error", error => console.error(error));
 
 // const subscribersRouter = require("./routes/subscribers");
 // app.use("/subscribers", subscribersRouter);
