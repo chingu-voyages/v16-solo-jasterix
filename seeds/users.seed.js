7da25192fe32c6ebff863199e11fc4c17855c811
Jasterix <ljcalixte@gmail.com>
2020-03-02 23:11:37 -0500

    crate users seed file

---
+++ seeds/users.seed.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: "Zell",
+    password: "12345678"
+  },
+  {
+    name: "Vincy",
+    password: "12345678"
+  },
+  {
+    name: "Shion",
+    password: "12345678"
+  }
+];
