faff60022bb3aaa5330d2988d13fe93f421c63a0
Jasterix <ljcalixte@gmail.com>
2020-03-05 22:47:18 -0500

    debugging

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const app = require("./app");
+const seedusers = require("./seeds/userSeed.js");
+const seedSpot = require("./seeds/spotSeed.js");
+
+
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
